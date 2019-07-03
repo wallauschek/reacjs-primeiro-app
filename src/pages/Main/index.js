93ad8e73d36fe9902013403ae6eaf8ae95c24629
FB-Pedro Wallauschek <pedro.wallauschek@gcel.local>
2019-07-03 16:46:35 -0300

    REstilizando página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title error>Hello world</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar novo repositório" />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
